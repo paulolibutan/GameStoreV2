362dd0655734b740c9d838a793990f46fe0d5841
paulolibutan <libutanpaulo@gmail.com>
2026-04-01 01:30:11 +0800

    Created games dto and sample list of games

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,33 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)
+    ),
+    new (
+        2,
+        "The Legend of Zelda: Ocarina of Time",
+        "Action-Adventure",
+        29.99M,
+        new DateOnly(1998, 11, 21)
+    ),
+    new (
+        3,
+        "Minecraft",
+        "Sandbox",
+        26.95M,
+        new DateOnly(2011, 11, 18)
+    )
+];
+
+// GET /games
+app.MapGet("/games", () => "Hello World!");
 
 app.Run();
