f1facbb4af1880a77a8b301133e9d8a1e76b51a5
paulolibutan <libutanpaulo@gmail.com>
2026-04-07 03:39:23 +0800

    feature(api): Added database context, entity framework core and database connection

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,16 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<GameStoreContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
