e66f16d7593afe1c2bc280ca8c9241acdc830c82
paulolibutan <libutanpaulo@gmail.com>
2026-04-07 04:10:41 +0800

    fix(api): Added missing save context

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddSqlServer<GameStoreContext>(connectionString, optionsAction:
                 new Genre { Name = "Sports" }
             );
         }
+        context.SaveChanges();
     });
 });
 
