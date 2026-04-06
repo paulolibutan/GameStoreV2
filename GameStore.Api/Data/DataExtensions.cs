c7070953fb90dbf57582e940bbdefd772de71014
paulolibutan <libutanpaulo@gmail.com>
2026-04-07 04:17:02 +0800

    refactor(api): Move db context logic to data extensions file

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -9,5 +10,29 @@ public static class DataExtensions
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDbContext(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        builder.Services.AddSqlServer<GameStoreContext>(connectionString, optionsAction: options =>
+        {
+            options.UseSeeding((context, _) =>
+            {
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Action" },
+                        new Genre { Name = "Adventure" },
+                        new Genre { Name = "RPG" },
+                        new Genre { Name = "Strategy" },
+                        new Genre { Name = "Sports" }
+                    );
+                }
+                context.SaveChanges();
+            });
+        });
     }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,32 +1,11 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
-using GameStore.Api.Models;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-builder.Services.AddSqlServer<GameStoreContext>(connectionString, optionsAction: options =>
-{
-    options.UseSeeding((context, _) =>
-    {
-        if (!context.Set<Genre>().Any())
-        {
-            context.Set<Genre>().AddRange(
-                new Genre { Name = "Action" },
-                new Genre { Name = "Adventure" },
-                new Genre { Name = "RPG" },
-                new Genre { Name = "Strategy" },
-                new Genre { Name = "Sports" }
-            );
-        }
-        context.SaveChanges();
-    });
-});
+builder.AddGameStoreDbContext();
 
 var app = builder.Build();
 
